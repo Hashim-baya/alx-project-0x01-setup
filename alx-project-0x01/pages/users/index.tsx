a76431eeb16540a331ff3a33efa9c3c4634f96f7
hashim-baya <bayahashim40@gmail.com>
2025-07-15 23:56:32 +0300

    Updated the users page

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -3,17 +3,17 @@ import { UserProps } from "@/interfaces"
 import UserCard  from "@/components/common/UserCard";
 
 export interface UsersProps {
-    users: UserProps[];
+    posts: UserProps[];
 }
-const Users: React.FC<UsersProps> = ({ users}) => {
-    console.log(users);
+const Users: React.FC<UsersProps> = ({ posts}) => {
+    console.log(posts);
     return (
         <div className="flex flex-col h-screen">
             <Header />
             <main>
                 <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-2">
                     {
-                        users?.map(({ id, name, username, email, address, phone, website, company }: UserProps, key: number) => (
+                        posts?.map(({ id, name, username, email, address, phone, website, company }: UserProps, key: number) => (
                             <UserCard id={id} name={name} username={username} email={email} address={address} phone={phone} website={website} company={company} key={key} />
                         ))
                     }
@@ -26,11 +26,11 @@ const Users: React.FC<UsersProps> = ({ users}) => {
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const users = await response.json()
+  const posts = await response.json()
 
   return {
     props: {
-      users
+      posts
     }
   }
 }
